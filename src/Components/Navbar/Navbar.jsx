492e5fc92f5a7be3ebb1154a80d7ce35949277c9
amshal1 <panshal911@gmail.com>
2023-07-25 10:22:22 +0530

    Navbar created

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./Navbar.module.css";
+const Navbar = () => {
+  return (
+    <div className={style["main-navbar-container"]}>
+      <div className={style["heading"]}>
+        <h1>Images</h1>
+      </div>
+      <div className={style["links"]}>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">All Images</a>
+          </li>
+          <li>
+            <a href="#">Profile</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
