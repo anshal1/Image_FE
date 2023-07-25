c4c41a36eb2429cac8ba057732a8950a72d31d1f
amshal1 <panshal911@gmail.com>
2023-07-25 10:56:31 +0530

    Login Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import Card from "./Components/Card/Card";
 import Navbar from "./Components/Navbar/Navbar";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login";
 function App() {
   return (
     <>
-      <Navbar />
-      <Card
-        img={
-          "https://cdn.vox-cdn.com/thumbor/KlIlVaMxSyV61YxibUZhaTAcf7k=/0x0:3840x2160/1200x675/filters:focal(1613x773:2227x1387)/cdn.vox-cdn.com/uploads/chorus_image/image/69919398/Pokemon_UNITE___Team_Up._Take_Down.___Screenshot_1.0.png"
-        }
-      />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Card
+                img={
+                  "https://cdn.vox-cdn.com/thumbor/KlIlVaMxSyV61YxibUZhaTAcf7k=/0x0:3840x2160/1200x675/filters:focal(1613x773:2227x1387)/cdn.vox-cdn.com/uploads/chorus_image/image/69919398/Pokemon_UNITE___Team_Up._Take_Down.___Screenshot_1.0.png"
+                }
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import style from "./Login.module.css";
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className={style["main-container"]}>
+      <div className={style["input-container"]}>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input type="text" className="inputs-global" name="name" />
+        <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input type="email" className="inputs-global" name="email" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input type="password" className="inputs-global" name="password" />
+      </div>
+      <div className={style["button-container"]}>
+        <button className="button-global">Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
