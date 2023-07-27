2aa59c477df5f51de2c891994c44f405b4cad369
amshal1 <panshal911@gmail.com>
2023-07-27 12:10:18 +0530

    Icons Added

--- src/Components/FullScreenImage/FullScreenImage.jsx
+++ src/Components/FullScreenImage/FullScreenImage.jsx
@@ -5,7 +5,15 @@ const FullScreenImage = ({ image, show, handleClose }) => {
     <div className={style[show ? "image-container" : "image-container-hide"]}>
       <div className={style["close-btn"]}>
         <button className="button-global" onClick={handleClose}>
-          Close
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="28"
+            viewBox="0 -960 960 960"
+            width="28"
+            fill="#fff"
+          >
+            <path d="M333-200v-133H200v-60h193v193h-60Zm234 0v-193h193v60H627v133h-60ZM200-567v-60h133v-133h60v193H200Zm367 0v-193h60v133h133v60H567Z" />
+          </svg>
         </button>
       </div>
       <img src={image} alt="" />
