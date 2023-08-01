6d77cc6d89cd27a30533b58b5a40c5bc52045dce
amshal1 <panshal911@gmail.com>
2023-08-01 09:25:00 +0530

    Alert Created

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Register from "./Pages/Register/Register";
 import Upload from "./Pages/Upload/Upload";
 import Home from "./Pages/Home/Home";
 import Alert from "./Components/Alert/Alert";
+import Context from "./Context/Context";
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Alert />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/upload" element={<Upload />} />
-        </Routes>
-      </Router>
+      <Context>
+        <Router>
+          <Navbar />
+          <Alert />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/upload" element={<Upload />} />
+          </Routes>
+        </Router>
+      </Context>
     </>
   );
 }
